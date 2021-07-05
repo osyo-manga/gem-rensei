64ade88970b93cc766f787ca2fbd44cf14b8a267
manga_osyo <manga.osyo@gmail.com>
2021-07-05 23:13:22 +0900

    Drop Hash with position.

--- lib/rensei/node_to_hash.rb
+++ lib/rensei/node_to_hash.rb
@@ -14,40 +14,25 @@ module Rensei
           NilClass,
           FalseClass,
           TrueClass,
-          Array,
-          Symbol,
           Numeric,
+          String,
+          Symbol,
+          Array,
           Regexp,
         ].each { |klass|
           refine klass do
-            def to_hashable
+            def to_h
               self
             end
           end
         }
       }
 
-      def to_hashable(ignore_codeposition: true, &block)
-        block = :to_hashable unless block
+      def to_h
         {
           type: type,
-          children: children.map(&block),
-        }.tap { |it|
-          it.merge!(
-            first_column: first_column,
-            last_column:  last_column,
-            first_lineno: first_lineno,
-            last_lineno:  last_lineno
-          ) unless ignore_codeposition
+          children: children.map(&:to_h),
         }
-      end
-
-      def to_json(ignore_codeposition: true)
-        to_hashable(ignore_codeposition: ignore_codeposition, &:to_json)
-      end
-
-      def to_h(ignore_codeposition: true)
-        to_hashable(ignore_codeposition: ignore_codeposition)
       end
     end
   end
