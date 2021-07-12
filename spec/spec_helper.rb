fe24cdf18a8f9d069af84ca3b0a7f2c31968ab86
manga_osyo <manga.osyo@gmail.com>
2021-07-13 00:01:59 +0900

    Disable experimental warning.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ require "bundler/setup"
 require "rensei"
 require "super_diff/rspec"
 
+if Warning.respond_to?(:[]=)
+  Warning[:experimental] = false
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
