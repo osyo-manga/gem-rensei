d036a30f5bdaf05956c87687d438f01e359514f1
manga_osyo <manga.osyo@gmail.com>
2021-07-12 21:24:06 +0900

    Fix support ruby versions.

--- bundle_install_all.sh
+++ bundle_install_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
-RBENV_VERSION=2.6.7 bundle install
-RBENV_VERSION=2.7.3 bundle install
+RBENV_VERSION=2.6.4 bundle install
+RBENV_VERSION=2.7.0 bundle install
+RBENV_VERSION=2.7.2 bundle install
 RBENV_VERSION=3.0.1 bundle install
 RBENV_VERSION=3.1.0-dev bundle install
--- rake_spec_all.sh
+++ rake_spec_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
-RBENV_VERSION=2.6.7 bundle exec rake spec
-RBENV_VERSION=2.7.3 bundle exec rake spec
+RBENV_VERSION=2.6.4 bundle exec rake spec
+RBENV_VERSION=2.7.0 bundle exec rake spec
+RBENV_VERSION=2.7.2 bundle exec rake spec
 RBENV_VERSION=3.0.1 bundle exec rake spec
 RBENV_VERSION=3.1.0-dev bundle exec rake spec
