47791333953686bb3b70622c43fc0aa75ab1b71f
manga_osyo <manga.osyo@gmail.com>
2021-08-04 23:05:34 +0900

    Add support versions.

--- bundle_install_all.sh
+++ bundle_install_all.sh
@@ -2,5 +2,8 @@
 RBENV_VERSION=2.6.4 bundle install
 RBENV_VERSION=2.7.0 bundle install
 RBENV_VERSION=2.7.2 bundle install
+RBENV_VERSION=2.7.3 bundle install
+RBENV_VERSION=2.7.4 bundle install
+RBENV_VERSION=3.0.0 bundle install
 RBENV_VERSION=3.0.1 bundle install
 RBENV_VERSION=3.1.0-dev bundle install
