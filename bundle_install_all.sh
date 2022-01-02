fab5246e2e45b97a845d9237dcc38bdefa675982
manga_osyo <manga.osyo@gmail.com>
2022-01-02 21:19:59 +0900

    Update CI versions.

--- bundle_install_all.sh
+++ bundle_install_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
-RBENV_VERSION=2.6.4 bundle install
+RBENV_VERSION=2.6.9 bundle install
 RBENV_VERSION=2.7.0 bundle install
 RBENV_VERSION=2.7.2 bundle install
 RBENV_VERSION=2.7.3 bundle install
-RBENV_VERSION=2.7.4 bundle install
-RBENV_VERSION=3.0.0 bundle install
-RBENV_VERSION=3.0.1 bundle install
-RBENV_VERSION=3.1.0-dev bundle install
+RBENV_VERSION=2.7.5 bundle install
+RBENV_VERSION=3.0.3 bundle install
+RBENV_VERSION=3.1.0 bundle install
+RBENV_VERSION=3.2.0-dev bundle install
--- rake_spec_all.sh
+++ rake_spec_all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -ex
-RBENV_VERSION=2.6.4 bundle exec rake spec
+RBENV_VERSION=2.6.9 bundle exec rake spec
 RBENV_VERSION=2.7.0 bundle exec rake spec
 RBENV_VERSION=2.7.2 bundle exec rake spec
-RBENV_VERSION=3.0.1 bundle exec rake spec
-RBENV_VERSION=3.1.0-dev bundle exec rake spec
+RBENV_VERSION=2.7.3 bundle exec rake spec
+RBENV_VERSION=2.7.5 bundle exec rake spec
+RBENV_VERSION=3.0.0 bundle exec rake spec
+RBENV_VERSION=3.0.3 bundle exec rake spec
+RBENV_VERSION=3.1.0 bundle exec rake spec
+RBENV_VERSION=3.2.0-dev bundle exec rake spec
